3589609b88d8b236beddbe633fe949dba49150c3
DBorisov <Denis.Borisov@billing.ru>
2016-04-12 17:49:01 +0300

    Задание 2

--- sandbox/src/main/java/ru/stesting/jtraining/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stesting/jtraining/sandbox/HelloWorld.java
@@ -3,7 +3,18 @@ package ru.stesting.jtraining.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    hello("world");
+  //  hello("world");
+  //
+  //  Задание 2
+  //
+    Point p1 = new Point(-4, -3);
+    Point p2 = new Point( 4,  3);
+    System.out.println("Расстояние между точками P1(" + p1.x + ", " + p1.y + ") и P2(" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
+
+    System.out.println("Расстояние между точками P1(" + p1.x + ", " + p1.y + ") и P2(" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
+    System.out.println("Расстояние между точками P1(" + p1.x + ", " + p1.y + ") и P2(" + p2.x + ", " + p2.y + ") = " + p2.distance(p1));
+
+
   }
 
   public static void hello(String somebody) {
@@ -14,6 +25,10 @@ public class HelloWorld {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
   }
 
 }

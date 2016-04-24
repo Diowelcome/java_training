2288f8083c0ec432df1d1851f1765fc497c0eebc
DBorisov <Denis.Borisov@billing.ru>
2016-04-24 17:48:48 +0300

    Задание 4

---
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/ContactCreationTests.java
@@ -0,0 +1,83 @@
+package ru.stesting.jtraining.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ContactCreationTests {
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  @Test
+  public void ContactCreationTests() {
+    gotoEditAddContactPage();
+    fillContractForm(new ShortContactData("Alexei", "Barancev", "barancev@gmail.com"));
+    submitContactInfo();
+    returnToHomePage();
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void submitContactInfo() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillContractForm(ShortContactData shortContactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(shortContactData.getFirstname());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(shortContactData.getLastname());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(shortContactData.getEmail());
+  }
+
+  private void gotoEditAddContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  private void login(String username, String password) {
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).sendKeys("\\undefined");
+    wd.findElement(By.id("content")).click();
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}

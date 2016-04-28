835d93605062dde82e5ca14840e6967a2f399ad1
DBorisov <Denis.Borisov@billing.ru>
2016-04-29 00:09:01 +0300

    Revert "Revert "Задание 8""
    This reverts commit e10972b75aed8a02e05cfa234da1e556897912b9.

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/TestBase.java
@@ -10,7 +10,7 @@ import ru.stesting.jtraining.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {

85659051cf0317fe4be68c5c579fdf0c802eff58
DBorisov <Denis.Borisov@billing.ru>
2016-04-28 12:52:26 +0300

    Задание 7: запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/HelperBase.java
@@ -2,15 +2,16 @@ package ru.stesting.jtraining.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Dborisov on 26.04.2016.
  */
 public class HelperBase {
-  protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+  public HelperBase(WebDriver wd) {
     this.wd = wd;
   }
 

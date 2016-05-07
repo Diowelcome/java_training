268d84a05ecaa9fcd1006e4ccbd3dd4ec4ea2306
DBorisov <Denis.Borisov@billing.ru>
2016-05-07 14:31:24 +0300

    Выбор лемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
@@ -16,7 +16,7 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test group", "Group Header", "Group Footer"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test group modified", "Modified group header", "Modified group footer"));
     app.getGroupHelper().submitGroupModification();

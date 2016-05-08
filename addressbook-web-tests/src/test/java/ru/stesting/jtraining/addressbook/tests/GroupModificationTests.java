324b4254c72e7cc2a54866a8af276f0ea7be76d5
DBorisov <Denis.Borisov@billing.ru>
2016-05-08 18:57:38 +0300

    Сравнение списков

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stesting.jtraining.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Dborisov on 26.04.2016.
  */
@@ -12,16 +14,16 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("Test group", "Group Header", "Group Footer"));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test group modified", "Modified group header", "Modified group footer"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }

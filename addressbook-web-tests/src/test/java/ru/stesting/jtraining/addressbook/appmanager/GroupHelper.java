9781f43501670893f1ce7c0c286d771f2648d2c0
DBorisov <Denis.Borisov@billing.ru>
2016-05-21 15:35:09 +0300

    Hamcrest

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/GroupHelper.java
@@ -1,12 +1,13 @@
 package ru.stesting.jtraining.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stesting.jtraining.addressbook.model.GroupData;
+import ru.stesting.jtraining.addressbook.model.Groups;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by DBorisov on 24.04.2016.
@@ -39,8 +40,8 @@ public class GroupHelper extends HelperBase {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public void initGroupModification() {
@@ -58,16 +59,16 @@ public class GroupHelper extends HelperBase {
     returnToGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
@@ -80,16 +81,15 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  public Groups all() {
+    Groups groups = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element: elements) {
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
       String name = element.getText();
       groups.add(new GroupData().withId(id).withName(name));
     }
-
-
     return groups;
   }
+
 }
--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/GroupModificationTests.java
@@ -1,12 +1,18 @@
 package ru.stesting.jtraining.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stesting.jtraining.addressbook.model.GroupData;
+import ru.stesting.jtraining.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Dborisov on 26.04.2016.
@@ -16,28 +22,21 @@ public class GroupModificationTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("Test group"));
     }
   }
 
   @Test
   public void testGroupModification() {
-    List<GroupData> before = app.group().list();
-    int index = before.size() - 1;
+    Groups before = app.group().all();
+    GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
-            .withId(before.get(index).getId()).withName("Test group modified").withHeader("Modified group header").withFooter("Modified group footer");
-    app.group().modify(index, group);
-    List<GroupData> after = app.group().list();
-    Assert.assertEquals(after.size(), before.size());
-
-    before.remove(index);
-    before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    Assert.assertEquals(before, after);
-    // Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+            .withId(modifiedGroup.getId()).withName("Test group modified").withHeader("Modified group header").withFooter("Modified group footer");
+    app.group().modify(group);
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size());
+    assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
 }

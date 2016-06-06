936b0098d18b5c7b1cd6cf79bb3d6d72e2cc5c5d
DBorisov <Denis.Borisov@billing.ru>
2016-06-06 18:04:48 +0300

    Задание 15

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/tests/ContactCreationTests.java
@@ -1,28 +1,80 @@
 package ru.stesting.jtraining.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import ru.stesting.jtraining.addressbook.model.Contacts;
 import ru.stesting.jtraining.addressbook.model.ContactData;
+import ru.stesting.jtraining.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType()); //List<GroupData>.class
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/Bond.jpg");
-    ContactData contact = new ContactData().withFirstname("James").withLastname("Bond").withEmail("james.bond@007.com")
-            .withPhoto(photo);
+    //File photo = new File("src/test/resources/Bond.jpg");
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+            before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt())
+                    // Метод all получает с формы allEmails, а не email (2/3), поэтому заполняем данную переменную
+                    // В методе equals сравниваем allEmails, поэтому enail (2/3) можно не обнулять
+                    .withAllEmails(mergeEmails(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt())
+                    )))));
   }
 
+  protected String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> (s != null && ! s.equals("")))
+            .map(ContactCreationTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String email) {
+    return email.replaceAll("\\s", "");
+  }
 }

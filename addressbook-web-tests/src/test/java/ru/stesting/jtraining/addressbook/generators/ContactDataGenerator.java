7bf9ccf3dae84e92df08ca80e907f23cbf9b2b8b
DBorisov <Denis.Borisov@billing.ru>
2016-06-08 15:02:17 +0300

    Правильное закрытие файла

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/generators/ContactDataGenerator.java
@@ -60,18 +60,18 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> groups, File file) throws IOException {
     XStream xstream = new XStream();
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   private List<ContactData> generateContacts(int count) {

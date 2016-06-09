ab1d4aa79aab0f72e9d81ae39598a030a28696a2
DBorisov <Denis.Borisov@billing.ru>
2016-06-09 17:05:15 +0300

    Сравнение данных, полученных из БД

--- addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stesting/jtraining/addressbook/appmanager/ApplicationManager.java
@@ -28,6 +28,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -37,6 +38,9 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
+
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -68,5 +72,9 @@ public class ApplicationManager {
 
   public ContactHelper contact() {
     return contactHelper;
+  }
+
+  public DbHelper db() {
+    return dbHelper;
   }
 }
